405be219b09c5252c4436d175b081ebc110d8504
fitahw <eldenringpr@gmail.com>
2025-06-11 18:29:20 +0200

    aldlawdlakwdl

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
 # This code defines the URL patterns for the blog application.
 # The `urlpatterns` list routes URLs to views.
